ca3f14bd1565f2de642941eff0b76d9083e77ec4
Robotnik08 <sebastiaanheins@gmail.com>
2025-10-27 19:24:29 +0100

    Expression Parser Start

--- crates/ast/src/lib.rs
+++ crates/ast/src/lib.rs
@@ -42,6 +42,33 @@ macro_rules! skip_block {
     };
 }
 
+macro_rules! skip_block_backwards {
+    ($self:ident, $index:ident, $span:ident) => {
+        if let TokenKind::BracketClose(bracket_type) = &$self.tokens[$index].kind {
+            while $index > $span.0 {
+                $index -= 1;
+                if let TokenKind::BracketOpen(open_bracket_type) = &$self.tokens[$index].kind {
+                    if open_bracket_type == bracket_type {
+                        break;
+                    }
+                }
+            }
+
+            if $index <= $span.0 {
+                return Err(
+                    error::Error::new(
+                        error::ErrorKind::SyntaxError,
+                        "Unclosed bracket".to_string(),
+                        $self.source_name.clone().unwrap_or("".to_string()),
+                        get_line_column_len!($self.tokens[$index + 1], $self.tokens[$index + 1]),
+                        false
+                    )
+                )
+            }
+        }
+    };
+}
+
 pub struct Parser {
     tokens: Vec<dosato_lexer::Token>,
     source_name: Option<String>,
@@ -55,9 +82,60 @@ impl Parser {
     pub fn parse(&self) -> Result<Node, error::Error> {
         self.parse_node(NodeType::Program, (0, self.tokens.len()))
     }
+
+    pub fn encased_in_parentheses(&self, span: (usize, usize)) -> bool {
+        if span.1 - span.0 < 2 {
+            return false;
+        }
+
+        if let TokenKind::BracketOpen(BracketType::Parenthesis(_)) = &self.tokens[span.0].kind {
+            if let TokenKind::BracketClose(BracketType::Parenthesis(_)) = &self.tokens[span.1 - 1].kind {
+                return true;
+            }
+        }
+
+        false
+    }
     
     pub fn parse_node(&self, node_type: NodeType, span: (usize, usize)) -> Result<Node, error::Error> {
         match node_type {
+            NodeType::Expression => {
+                if self.encased_in_parentheses(span) {
+                    return self.parse_node(NodeType::Expression, (span.0 + 1, span.1 - 1));
+                }
+
+                if span.1 - span.0 == 1 {
+                    let token = &self.tokens[span.0];
+                    match &token.kind {
+                        TokenKind::Identifier(id) => {
+                            return Ok(Node::Identifier(0));
+                        }
+                        TokenKind::IntegerLiteral(_) | 
+                        TokenKind::NumberLiteral(_) | 
+                        TokenKind::StringLiteral(_) | 
+                        TokenKind::BooleanLiteral(_) | 
+                        TokenKind::CharLiteral(_) |
+                        TokenKind::Infinity |
+                        TokenKind::NaN |
+                        TokenKind::Null => {
+                            return Ok(Node::Literal(token.clone()));
+                        }
+
+                        _ => {
+                            return Err(
+                                error::Error::new(error::ErrorKind::SyntaxError, "Expected an expression".to_string(), self.source_name.clone().unwrap_or("".to_string()), get_line_column_len!(self.tokens[span.0], self.tokens[span.0]), false)
+                            );
+                        }
+                    }
+                } else if span.1 - span.0 == 0 {
+                    return Err(
+                        error::Error::new(error::ErrorKind::SyntaxError, "Empty expression".to_string(), self.source_name.clone().unwrap_or("".to_string()), get_line_column_len!(self.tokens[span.0], self.tokens[span.0]), false)
+                    );
+                }
+
+                Ok(Node::Blank)
+            }
+            
             NodeType::Program => {
                 let mut statements: Vec<Node> = Vec::new();
 
@@ -103,14 +181,51 @@ impl Parser {
                 };
 
                 if key_word_type.is_master_keyword() {
-                    // For now, just return a Blank node for master keywords
-                    Ok(Node::Blank)
+                    match key_word_type {
+                        dosato_lexer::KeyWord::Do => {
+                            let body = self.parse_node(NodeType::DoBody, (span.0 + 1, span.1))?;
+                            Ok(Node::Do { body: vec![body] })
+                        }
+                        _ => Ok(Node::Blank)
+                    }
                 } else {
                     Err(
                         error::Error::new(error::ErrorKind::SyntaxError, "Expected a master keyword at the start of statement".to_string(), self.source_name.clone().unwrap_or("".to_string()), get_line_column_len!(self.tokens[span.0], self.tokens[span.0]), false)
                     )
                 }
             }
+
+            NodeType::DoBody => {
+                let body = self.parse_node(NodeType::CallExpression, span)?;
+                Ok(Node::DoBody { body: Box::new(body) })
+            }
+
+            NodeType::CallExpression => {
+                let end_token = &self.tokens[span.1 - 1];
+                // must end with a closing parenthesis, everything inside until the first opening parenthesis are the arguments
+                // the rest up front is the callee AKA expression
+                if let TokenKind::BracketClose(BracketType::Parenthesis(_)) = &end_token.kind {
+                    let mut index = span.1 - 1;
+                    skip_block_backwards!(self, index, span);
+                    if let TokenKind::BracketOpen(BracketType::Parenthesis(_)) = &self.tokens[index].kind {
+                        let arguments = Node::Blank;
+                        let callee = self.parse_node(NodeType::Expression, (span.0, index))?;
+                        Ok(Node::CallExpression {
+                            callee: Box::new(callee),
+                            arguments: vec![arguments],
+                        })
+                    } else {
+                        Err(
+                            error::Error::new(error::ErrorKind::SyntaxError, "Expected opening parenthesis for call expression".to_string(), self.source_name.clone().unwrap_or("".to_string()), get_line_column_len!(self.tokens[index], self.tokens[index]), false)
+                        )
+                    }
+                } else {
+                    Err(
+                        error::Error::new(error::ErrorKind::SyntaxError, "Expected closing parenthesis for call expression".to_string(), self.source_name.clone().unwrap_or("".to_string()), get_line_column_len!(self.tokens[span.1 - 1], self.tokens[span.1 - 1]), false)
+                    )
+                }
+            }
+
             _ => {
                 Err(
                     error::Error::new(error::ErrorKind::SyntaxError, "Unknown node type".to_string(), self.source_name.clone().unwrap_or("".to_string()), get_line_column_len!(self.tokens[span.0], self.tokens[span.1 - 1]), false)
